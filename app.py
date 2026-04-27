59895ddd28bd3378995e96c08c7f2043175646b3
MarkJanczak <dar359@cornell.edu>
2026-04-26 21:55:36 -0400

    Task 4. Printed out whether the user has money left to spend or how much they are in debt or have left over

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ spent       = float(spent)
 earned      = input("How much did you earn this week from working?\n")
 earned      = float(earned)
 amount_left = earned - spent
+if amount_left < 0:
+        print (f"You have spent ${-amount_left:.2f} more than you have earned this week.")
+elif amount_left == 0:
+        print ("You have spent exactly what you have earned this week.")
+else:
+        print (f"You still have ${amount_left:.2f} to spend for the week or to put in savings!")
+
