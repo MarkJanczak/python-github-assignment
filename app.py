13300586f9b0f4f61a482875e2a44e9e2c099002
MarkJanczak <dar359@cornell.edu>
2026-04-26 21:50:18 -0400

    Task 3. Added calculation based on user input by tracking how much they have spent and how much they have earned.

--- app.py
+++ app.py
@@ -1,1 +1,5 @@
-spent = input("How much did you spend this week on groceries?\n")
+spent       = input("How much did you spend this week on groceries?\n")
+spent       = float(spent)
+earned      = input("How much did you earn this week from working?\n")
+earned      = float(earned)
+amount_left = earned - spent
